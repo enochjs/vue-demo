feb13ada9742f1f17f59d1936d264f0852b19900
王永东 <fenghe@wangyongdongdeMacBook-Pro.local>
2025-03-14 22:28:03 +0800

    feat(main): add ref demo

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,52 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+// define props
+const name = ref('张三')
+const age = ref(18)
+const address = ref('北京')
+
+// define methods
+const changeName = () => {
+  name.value = '李四'
+}
+const changeAge = () => {
+  age.value = 20
+}
+const changeAddress = () => {
+  address.value = '上海'
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div class="wrapper">
+    <div class=" text-center">
+      姓名：{{ name }}
     </div>
-  </header>
-
-  <RouterView />
+    <div>
+      年龄：{{ age }}
+    </div>
+    <div>
+      地址：{{ address }}
+    </div>
+  </div>
+  <div>
+    <button @click="changeName">
+      修改姓名
+    </button>
+    <button @click="changeAge">
+      修改年龄
+    </button>
+    <button @click="changeAddress">
+      修改地址
+    </button>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.wrapper {
+  margin: 20px;
+  padding: 20px;
+  border: 1px solid #ccc;
 }
 </style>
