e6c0b3f47ce3d54abbb8f609b9805982ce368b9c
王永东 <fenghe@wangyongdongdeMacBook-Pro.local>
2025-03-15 10:53:58 +0800

    feat(main): add watch demo

---
+++ backup/watch-reactive/App.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { reactive, watch } from 'vue';
+
+
+const person = reactive({
+  name: '张三',
+  age: 18
+})
+
+const changePerson = () => {
+  person.name += '~'
+}
+
+const changeAge = () => {
+  person.age += 1
+}
+
+const stop = watch(person, (newValue, oldValue) => {
+  console.log('====', newValue, oldValue)
+}, {
+  // immediate: true,
+  deep: true
+})
+
+</script>
+
+<template>
+  <div class="person">
+    <h2>姓名：{{ person.name }}</h2>
+    <h2>年龄：{{ person.age }}</h2>
+    <button @click="changePerson">修改名字</button>
+    <button @click="changeAge">修改年龄</button>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  margin: 20px;
+  padding: 20px;
+  border: 1px solid #ccc;
+}
+</style>
---
+++ backup/watch-ref-obj/App.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+
+
+const person = ref({
+  name: '张三',
+  age: 18
+})
+
+const changePerson = () => {
+  person.value.name += '~'
+}
+
+const changeAge = () => {
+  person.value.age += 1
+}
+
+const stop = watch(person, (newValue, oldValue) => {
+  console.log('====', newValue, oldValue)
+}, {
+  // immediate: true,
+  deep: true
+})
+
+</script>
+
+<template>
+  <div class="person">
+    <h2>姓名：{{ person.name }}</h2>
+    <h2>年龄：{{ person.age }}</h2>
+    <button @click="changePerson">修改名字</button>
+    <button @click="changeAge">修改年龄</button>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  margin: 20px;
+  padding: 20px;
+  border: 1px solid #ccc;
+}
+</style>
