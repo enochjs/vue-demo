4d4337b0d1eb0319d62f33366a3d7c9330cd25bb
王永东 <fenghe@wangyongdongdeMacBook-Pro.local>
2025-03-23 23:34:10 +0800

    feat(main): route function add

--- src/pages/News.vue
+++ src/pages/News.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts" name="News">
-import { reactive } from 'vue';
+import { onMounted, reactive } from 'vue';
+import {  useRouter } from 'vue-router';
+
+const router = useRouter();
 
 const newsList = reactive([
   {
@@ -19,11 +22,37 @@ const newsList = reactive([
   }
 ])
 
+onMounted(() => {
+  console.log('News mounted');
+  setTimeout(() => {
+    router.push({
+      name: 'Detail',
+      params: {
+        id: 3,
+        title: 'news3',
+        content: 'news3 content'
+      }
+    })
+  }, 3000);
+})
+
+const handleDetail = (news: any) => {
+  router.push({
+    name: 'Detail',
+    params: {
+      id: news.id,
+      title: news.title,
+      content: news.content
+    }
+  })
+}
+
 </script>
 
 <template>
   <ul>
     <li v-for="news in newsList" :key="news.id">
+      <button @click="handleDetail(news)">查看详情</button>
       <!-- <RouterLink :to="`/news/detail?id=${ news.id }&title=${news.title}&content=${news.content}`">{{ news.title }}</RouterLink> -->
        <RouterLink :to="{
         // path: `/news/detail/${ news.id }/${ news.title }/${ news.content }`,
