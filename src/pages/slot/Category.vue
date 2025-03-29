a242c2edbbec820cdd078cf8f49b47c2d745f511
王永东 <fenghe@wangyongdongdeMacBook-Pro.local>
2025-03-29 22:07:07 +0800

    feat(main): slot add

---
+++ src/pages/slot/Category.vue
@@ -0,0 +1,21 @@
+<template>
+<h3>{{ title }}</h3>
+<div class="content">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+defineProps(['title']);
+</script>
+<style scoped>
+.father {
+  flex: 1;
+  border: 1px solid #ccc;
+  padding: 10px;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  margin-right: 10px;
+}
+</style>
---
+++ src/pages/slot/parent.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="father">
+    <div class="content">
+      <Category title="test">
+        <ul>
+          <li v-for="item in gameList" :key="item.id">
+            <div>{{ item.name }}</div>
+          </li>
+        </ul>
+      </Category>
+    </div>
+    <div class="content">
+      <Category title="test">
+        <ol>
+          <li v-for="item in gameList" :key="item.id">
+            <div>{{ item.name }}</div>
+          </li>
+        </ol>
+      </Category>
+    </div>
+    <div class="content">
+      <Category title="test">
+          <h3 v-for="item in gameList" :key="item.id">
+            <div>{{ item.name }}</div>
+          </h3>
+      </Category>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { reactive } from 'vue';
+import Category from './Category.vue';
+
+const gameList = reactive([
+  { id: 1, name: '游戏1' },
+  { id: 2, name: '游戏2' },
+  { id: 3, name: '游戏3' },
+]);
+
+</script>
+<style scoped>
+.father {
+  display: flex;
+  flex: 1;
+  border: 1px solid #ccc;
+  padding: 10px;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  margin-right: 10px;
+  /* flex-direction: ; */
+  justify-content: space-between;
+}
+.content {
+  flex: 1;
+  height: 400px;
+  border: 1px solid #ccc;
+  padding: 10px;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  margin-right: 10px;
+}
+</style>
