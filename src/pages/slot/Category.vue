53675d54582dd727a1f0bf4ed5b6c11910004d2e
王永东 <fenghe@wangyongdongdeMacBook-Pro.local>
2025-03-29 22:07:07 +0800

    feat(main): slot add

---
+++ src/pages/slot/Category.vue
@@ -0,0 +1,45 @@
+<!--
+默认插槽
+<template>
+<h3>{{ title }}</h3>
+<div class="content">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+defineProps(['title']);
+</script>
+<style scoped>
+.father {
+  flex: 1;
+  border: 1px solid #ccc;
+  padding: 10px;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  margin-right: 10px;
+}
+</style> -->
+
+<template>
+  <h3>{{ title }}</h3>
+  <div class="content">
+      <slot name="slot"></slot>
+    </div>
+  </template>
+
+  <script setup lang="ts">
+  import { defineProps } from 'vue';
+  defineProps(['title']);
+  </script>
+  <style scoped>
+  .father {
+    flex: 1;
+    border: 1px solid #ccc;
+    padding: 10px;
+    border-radius: 5px;
+    margin-bottom: 10px;
+    margin-right: 10px;
+  }
+  </style>
---
+++ src/pages/slot/parent.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="father">
+    <div class="content">
+      <Category title="test">
+        <template v-slot:slot>
+          <ul>
+            <li v-for="item in gameList" :key="item.id">
+              <div>{{ item.name }}</div>
+            </li>
+          </ul>
+        </template>
+      </Category>
+    </div>
+    <div class="content">
+      <Category title="test">
+        <template v-slot:slot>
+          <ol>
+            <li v-for="item in gameList" :key="item.id">
+              <div>{{ item.name }}</div>
+            </li>
+          </ol>
+        </template>
+      </Category>
+    </div>
+    <div class="content">
+        <Category title="test">
+          <template v-slot:slot>
+            <h3 v-for="item in gameList" :key="item.id">
+              <div>{{ item.name }}</div>
+            </h3>
+         </template>
+        </Category>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { reactive } from 'vue';
+import Category from './Category.vue';
+
+const gameList = reactive([
+  { id: 1, name: '游戏1' },
+  { id: 2, name: '游戏2' },
+  { id: 3, name: '游戏3' },
+]);
+
+</script>
+<style scoped>
+.father {
+  display: flex;
+  flex: 1;
+  border: 1px solid #ccc;
+  padding: 10px;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  margin-right: 10px;
+  /* flex-direction: ; */
+  justify-content: space-between;
+}
+.content {
+  flex: 1;
+  height: 400px;
+  border: 1px solid #ccc;
+  padding: 10px;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  margin-right: 10px;
+}
+</style>
